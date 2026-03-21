bd78a4a05fb4e88c043174c4f9f8acf082f8029b
Newrite <newrite.work@yandex.ru>
2026-03-21 17:22:23 +0700

    update pch

--- libskyrim/cpp/include/PCH.h
+++ libskyrim/cpp/include/PCH.h
@@ -3,6 +3,25 @@
 // Обязательно для правильной работы строк в CommonLib
 #pragma execution_character_set("utf-8")
 
+#include <cstdint>
+#include <cstddef>
+#include <array>
+#include <string>
+#include <string_view>
+#include <stdexcept>
+#include <compare>
+#include <optional>
+#include <format>
+#include <filesystem>
+#include <atomic>
+#include <mutex>
+#include <memory>
+#include <vector>
+#include <utility>
+#include <type_traits>
+#include <variant>
+#include <source_location>
+
 // Подключаем базовые вещи SKSE и Skyrim
 #include <RE/Skyrim.h>
 #include <SKSE/SKSE.h>
