77555108a31ac73222d39ea371e298a535e073df
Newrite <newrite.work@yandex.ru>
2026-03-21 20:01:20 +0700

    another build errors fix

--- libskyrim/build.rs
+++ libskyrim/build.rs
@@ -7,13 +7,25 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=commonlib_bridge");
-
-    // Xmake-пакеты тоже линкуются статически. Укажем Rust найти их
     println!("cargo:rustc-link-lib=static=commonlibsse-ng");
+    println!("cargo:rustc-link-lib=static=spdlog");
 
+    // === БАЗОВЫЕ БИБЛИОТЕКИ WINDOWS ===
     println!("cargo:rustc-link-lib=version");
     println!("cargo:rustc-link-lib=user32");
+    println!("cargo:rustc-link-lib=advapi32");   // Для RegGetValue (Реестр)
+    println!("cargo:rustc-link-lib=bcrypt");     // Для BCryptOpenAlgorithmProvider (Хэширование/Криптография)
+    println!("cargo:rustc-link-lib=ole32");      // Для COM (CoTaskMemFree и т.д.)
+    println!("cargo:rustc-link-lib=shell32");    // Для SHGetKnownFolderPath
+    println!("cargo:rustc-link-lib=dbghelp");    // Для отладки и stack trace
 
-    println!("cargo:rerun-if-changed=cpp/src/bridge.cpp");
+    // === DIRECTX ===
+    // CommonLibSSE-NG использует DirectX для рендера UI, хуков и прочего.
+    println!("cargo:rustc-link-lib=d3d11");
+    println!("cargo:rustc-link-lib=dxgi");
+    println!("cargo:rustc-link-lib=d3dcompiler"); // Для D3DCompile
+
+    println!("cargo:rerun-if-changed=cpp/src");
+    println!("cargo:rerun-if-changed=cpp/include");
     println!("cargo:rerun-if-changed=cpp/xmake.lua");
 }
